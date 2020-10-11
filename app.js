5db30f22a4f1f42370a6eb014c0ad1d40f67204f
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-10-11 13:01:12 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+const solarButton = document.getElementById('solar');
+const body = document.body;
+
+
+// Apply the cached theme on reload
+
+const theme = localStorage.getItem('theme');
+const isSolar = localStorage.getItem('isSolar');
+
+if (theme) {
+  body.classList.add(theme);
+  isSolar && body.classList.add('solar');
+}
+
+// Button Event Handlers
+
+darkButton.onclick = () => {
+  body.classList.replace('light', 'dark');
+  localStorage.setItem('theme', 'dark');
+};
+
+lightButton.onclick = () => {
+  body.classList.replace('dark', 'light');
+
+  localStorage.setItem('theme', 'light');
+};
+
+solarButton.onclick = () => {
+ 
+
+  if (body.classList.contains('solar')) {
+    
+    body.classList.remove('solar');
+  
+
+    localStorage.removeItem('isSolar');
+
+  } else {
+
+    body.classList.add('solar');
+
+    localStorage.setItem('isSolar', true);
+  }
+};
